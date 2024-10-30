364a6d58e4951a9de7ff1375bdd901316b1f9223
Migue Paez <shukaroo@gmail.com>
2024-10-30 08:24:53 -0500

    cursojavascript

---
+++ app.js
@@ -0,0 +1,43 @@
+//Comenntario de Linea
+/**comentario multilinea
+ */
+
+console.log"Hola Mundo");
+
+//Variables
+let numero1 = 45;
+let numero2 = 39;
+let pi = 3.14156;
+
+let estado = true;
+   
+let numero3=78;
+
+let direccion = "kr 147 # 150-26";
+
+let numero=125;
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(numero3);
+
+suma = numero1 + numero2;
+
+console.log(suma)
+//para sacar comillas invertidas alt + 96
+//para sacar comillas normal alt + 39  '
+//concatenar valores
+console.log('EL PRIMER VALOR ES:' + numero1);
+console.log('EL SEGUNDO VALOR ES:' + numero2);
+console.log('LA SUMA DE' + numero1 + 'Y' + numero2 'ES:' + suma);
+//EL mas se puede cambiar por comas y sigue sumando normnal todo
+console.log(`LA SUMA DE` , numero1 , `Y` , numero2 `ES:` , suma);
+//literal template"Es cuando colocamos las comillas invertidas``"
+console.log(`LA SUMA DE ${numero1} Y ${numero2} ES: ${suma}`);
+console.log(`LA RESTA DE ${numero1} Y ${numero2} ES: ${resta}`);
+console.log(`LA MULTIPLICACION DE ${numero1} Y ${numero2} ES: ${multiplicacion}`);
+
+
+
